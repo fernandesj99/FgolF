e490c4924e786c70bf3ee068e78b84d2203aff9c
fernandesj99 <joao.mribeiro.fernandes@gmail.com>
2025-04-19 00:01:02 +0100

    atualizar melhores momentos para channel youtube

--- src/data/resultados.ts
+++ src/data/resultados.ts
@@ -4,7 +4,7 @@ export const resultados = [
       nome: "Open Lisboa",
       data: "21 de Abril de 2025",
       slug: "open-lisboa",
-      video: "https://youtube.com/watch?v=xxxxxx", // link real aqui
+      video: "https://www.youtube.com/channel/UCvP7RKs1x7wBHc2TZ_VpkWQ", // link real aqui
       draw: "/pdfs/open-lisboa-draw.pdf",
       resultados: "/pdfs/open-lisboa-resultados.pdf"
     },
@@ -12,7 +12,7 @@ export const resultados = [
       nome: "Porto Masters",
       data: "3 de Maio de 2025",
       slug: "porto-masters",
-      video: "https://youtube.com/watch?v=yyyyyy",
+      video: "hhttps://www.youtube.com/channel/UCvP7RKs1x7wBHc2TZ_VpkWQ",
       draw: "/pdfs/porto-masters-draw.pdf",
       resultados: "/pdfs/porto-masters-resultados.pdf"
     },
@@ -20,7 +20,7 @@ export const resultados = [
       nome: "Algarve Challenge",
       data: "17 de Maio de 2025",
       slug: "algarve-challenge",
-      video: "https://youtube.com/watch?v=zzzzzz",
+      video: "https://www.youtube.com/channel/UCvP7RKs1x7wBHc2TZ_VpkWQ",
       draw: "/pdfs/algarve-draw.pdf",
       resultados: "/pdfs/algarve-resultados.pdf"
     }
